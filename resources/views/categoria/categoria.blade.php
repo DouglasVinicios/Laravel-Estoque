6fa9ed8bc756e6ee5eff6c1c21d841b843a4707f
Douglas vinicios <douglas.vinicios1@outlook.com>
2019-07-21 18:15:55 -0300

    Iniciando parte de categorias

--- resources/views/categoria/categoria.blade.php
+++ resources/views/categoria/categoria.blade.php
@@ -1,38 +1,34 @@
 @extends('layouts.principal')
 @section('conteudo')
-    <h3>Listagem de Produtos</h3>
+    <h3>Listagem de Categorias</h3>
     @if(old('nome'))
         <div class="alert alert-success">
-            <strong>Sucesso!</strong> O produto {{old('nome')}} foi adicionado.
+            <strong>Sucesso!</strong> A categoria {{old('nome')}} foi adicionado.
         </div>
     @endif
-    @if(empty($produtos))
-        <div class="alert alert-danger">Nenhum produto cadastrado</div>
+    @if(empty($categorias))
+        <div class="alert alert-danger">Nenhuma categoria cadastrado</div>
     @else
         <div class="table-responsive-sm">
             <table class="table table-hover table-bordered table-sm">
                 <thead>
                 <tr>
                     <th>Nome</th>
-                    <th>Valor</th>
                     <th>Descrição</th>
-                    <th>Quantidade</th>
                     <th>Ações</th>
                 </tr>
                 </thead>
                 <tbody>
-                @foreach($produtos as $p)
-                    <tr class="{{$p->quantidade<=1 ? 'table-danger' :''}}">
+                @foreach($categorias as $c)
+                    <tr class="table-info">
                         <td>{{ $p->nome }}</td>
-                        <td>{{ $p->valor }}</td>
-                        <td>{{ $p->descricao }}</td>
-                        <td>{{ $p->quantidade }}</td>
+                        <td>{{ $c->descricao }}</td>
                         <td>
                             <a class="btn btn-outline-info btn-sm"
-                               href="{{action('ProdutoController@mostra', $p->id)}}">Ver
+                               href="{{action('CategoriaController@mostra', $c->id)}}">Ver
                                 mais</a>
                             <a class="btn btn-outline-danger btn-sm"
-                               href="{{action('ProdutoController@remove', $p->id)}}">Remover</a>
+                               href="{{action('CategoriaController@remove', $c->id)}}">Remover</a>
                         </td>
                     </tr>
                 @endforeach
